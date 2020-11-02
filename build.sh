afd107fb67c70f91a9b5584e1510052b2ca87cea
starlight5234 <starlight5234@protonmail.ch>
2020-11-03 01:19:34 +0530

    Updoooooot zipping function
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- build.sh
+++ build.sh
@@ -142,7 +142,11 @@ fi
 cd $ZIP_DIR
 make clean &>/dev/null
 cp $KERN_IMG $ZIP_DIR/zImage
-make zip &>/dev/null
+NAME="StormBreaker-Kernel"
+DATE=$(date "+%d%m%Y-%I%M")
+STORM_ZIP_NAME=${NAME}-${KERN_VER}-${DEVICE}-${DATE}.zip
+EXCLUDE="Storm* *placeholder* .git"
+zip -r9 "$STORM_ZIP_NAME" . -x $EXCLUDE &> /dev/null
 ls
 ZIP=$(echo *.zip)
 tg_pushzip
