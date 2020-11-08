a17e0996fc7d65df1890abb303adc4f2eca178d4
starlight5234 <starlight5234@protonmail.ch>
2020-11-08 16:15:41 +0530

    Mochi going fix fix
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ export KBUILD_BUILD_USER="StormbreakerCI-BOT"
 export GCC_COMPILE="$GCC" 
 export CLANG_VER="$clang_ver"
 export KBUILD_BUILD_HOST="Stormbreaker-HQ"
-export REVISION="no-boobs-cam-bq"
+export REVISION="no-boobs-cam-mochi"
 
 #==============================================================
 #===================== Function Definition ====================
