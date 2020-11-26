261350427eb6367b0f59a2976050e97160e785d4
starlight5234 <starlight5234@protonmail.ch>
2020-11-26 20:19:04 +0530

    cam
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ export KBUILD_BUILD_USER="StormbreakerCI-BOT"
 export GCC_COMPILE="yes" 
 export CLANG_VER="$clang_ver"
 export KBUILD_BUILD_HOST="Stormbreaker-HQ"
-export REVISION="R-compiler-fixes"
+export REVISION="R-camera"
 
 #==============================================================
 #===================== Function Definition ====================
