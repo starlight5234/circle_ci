453b7bd75624ee612f04404b3628377c6a9f803c
starlight5234 <starlight5234@protonmail.ch>
2020-11-26 20:57:36 +0530

    compile faster u rart
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ export KBUILD_BUILD_USER="StormbreakerCI-BOT"
 export GCC_COMPILE="yes" 
 export CLANG_VER="$clang_ver"
 export KBUILD_BUILD_HOST="Stormbreaker-HQ"
-export REVISION="R-camera-vince-idle"
+export REVISION="R-fast-compile"
 
 #==============================================================
 #===================== Function Definition ====================
