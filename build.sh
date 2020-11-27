efc74103c5c43ca6981dcf1949ce3c045a2cd460
starlight5234 <starlight5234@protonmail.ch>
2020-11-27 16:14:59 +0530

    Kbhai
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ export KBUILD_BUILD_USER="StormbreakerCI-BOT"
 export GCC_COMPILE="yes" 
 export CLANG_VER="$clang_ver"
 export KBUILD_BUILD_HOST="Stormbreaker-HQ"
-export REVISION="R-debugftest"
+export REVISION="R-debugfs-test"
 
 #==============================================================
 #===================== Function Definition ====================
