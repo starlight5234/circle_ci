82ea85d11255e0c0b91202a0ee0a4b223eeca5ad
starlight5234 <starlight5234@protonmail.ch>
2020-12-02 16:01:02 +0530

    sched
    Change-Id: Ideb22566242e6aa26daa3d2743af34c5f74ed8f9
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ export KBUILD_BUILD_USER="StormbreakerCI-BOT"
 export GCC_COMPILE="yes" 
 export CLANG_VER="$clang_ver"
 export KBUILD_BUILD_HOST="Stormbreaker-HQ"
-export REVISION="R-sched-5"
+export REVISION="R-sched-ok"
 
 #==============================================================
 #===================== Function Definition ====================
