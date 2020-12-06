9edf73e85a8578e573c767eee8fa08c129bd5b91
starlight5234 <starlight5234@protonmail.ch>
2020-12-06 18:44:54 +0530

    Some boost
    Change-Id: If46d8d81fe25a19e366594ba0e4d688a8278f6a1
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ export KBUILD_BUILD_USER="StormbreakerCI-BOT"
 export GCC_COMPILE="yes" 
 export CLANG_VER="$clang_ver"
 export KBUILD_BUILD_HOST="Stormbreaker-HQ"
-export REVISION="R-sched-16ms"
+export REVISION="R-boobs-16ms"
 
 #==============================================================
 #===================== Function Definition ====================
